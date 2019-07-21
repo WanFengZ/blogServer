5a012591ecddabdd3ac6631d4faa7a0de19f9b4b
WanFengZ <1604574606@qq.com>
2019-07-21 17:28:17 +0800

    add user: login(session)

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
+const SESSION_DATA = {}
+
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    return d.toUTCString()
+}
 
 const handlePostData = (req) => {
     return new Promise((resolve, reject) => {
@@ -40,6 +47,31 @@ const serverHandle = (req, res) => {
     req.path = url.split('?')[0]
     //解析query
     req.query = querystring.parse(url.split('?')[1])
+    //解析cookie
+    const cookieStr = req.headers.cookie || '';
+    req.cookie = {}
+    cookieStr.split(';').forEach((item) => {
+        if (!item) {
+            return
+        }
+        const arr = item.split('=')
+        const key = arr[0].trim()
+        const val = arr[1].trim()
+        req.cookie[key] = val
+    })
+    //解析section
+    let needSetCookie = false
+    let sessionId = req.cookie.sessionid
+    if (sessionId) {
+        if(!SESSION_DATA[sessionId]) {
+            SESSION_DATA[sessionId] = {}
+        }
+    } else {
+        needSetCookie = true
+        sessionId = `${Date.now()}_${Math.random()}`
+        SESSION_DATA[sessionId] = {}
+    }
+    req.session = SESSION_DATA[sessionId]
     //处理post数据
     handlePostData(req).then((postData) => {
         //储存post数据
@@ -48,14 +80,22 @@ const serverHandle = (req, res) => {
         const blogResult = handleBlogRouter(req, res)
         if (blogResult) {
             blogResult.then((blogData) => {
+                if (needSetCookie) {
+                    res.setHeader('Set-Cookie', `sessionid=${sessionId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+                }
                 res.end(JSON.stringify(blogData))
             })
             return
         }
         //处理user路由
-        const userData = handleUserRouter(req, res)
-        if (userData) {
-            res.end(JSON.stringify(userData))
+        const userResult = handleUserRouter(req, res)
+        if (userResult) {
+            userResult.then((userData) => {
+                if (needSetCookie) {
+                    res.setHeader('Set-Cookie', `sessionid=${sessionId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+                }
+                res.end(JSON.stringify(userData))
+            })
             return
         }
         //未命中路由
--- src/router/user.js
+++ src/router/user.js
@@ -1,17 +1,38 @@
 const { login } =require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
+
+
 const handleUserRouter = (req, res) => {
     const method = req.method
     const path = req.path
     //登陆接口
-    if (method === 'POST' && path === '/api/user/login') {
-        const { username, password } = req.body
+    if (method === 'GET' && path === '/api/user/login') {
+        // const { username, password } = req.body
+        const username = req.query.username
+        const password = req.query.password
         const result = login(username, password)
-        if (result) {
-            return new SuccessModel()
+        return result.then((data) => {
+            if (data.username) {
+                req.session.userName = data.username
+                req.session.realName = data.realname
+                return new SuccessModel(data)
+            } else {
+                return new ErrorModel('登陆失败')
+            }
+        })
+    }
+    if (method === 'GET' && path === '/api/user/login-test') {
+        if (req.cookie.username) {
+            return Promise.resolve(
+                new SuccessModel({
+                    username: req.session.username
+                })
+            )
         } else {
-            return new ErrorModel('登陆失败')
+            return Promise.resolve(
+                new ErrorModel('尚未登陆')
+            )
         }
     }
 }
